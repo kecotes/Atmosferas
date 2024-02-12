fa849a0872673bb54996d77a9ec97b94b6d272dc
kecotes <kecotes@uniguajira.edu.co>
2024-02-12 07:59:55 -0500

    Front y backend inicializado

---
+++ AtmoferasApi/src/main/java/com/application/res/AtmoferasApiApplication.java
@@ -0,0 +1,13 @@
+package com.application.res;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmoferasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmoferasApiApplication.class, args);
+	}
+
+}
---
+++ AtmoferasApi/src/test/java/com/application/res/AtmoferasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.res;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmoferasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
